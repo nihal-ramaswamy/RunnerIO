5c31121fcfe5ae32d62be1daa7d1b4c10450f419
Nihal Ramaswamy <nihalramaswamy@gmail.com>
2025-07-24 07:23:26 +0530

    Update Go version in workflow
    Use Go version 1.24.2 in the CI workflow.

---
+++ internal/services/area_test.go
@@ -0,0 +1,72 @@
+package services
+
+import (
+	"testing"
+
+	dtoschema "github.com/nihal-ramaswamy/RunnerIO/internal/dto/schema"
+)
+
+func TestArea1(t *testing.T) {
+	polygonData := []dtoschema.PolygonData{
+		{
+			Runner: "nihal",
+			Coords: []dtoschema.CoordinateStruct{
+				{X: 0, Y: 0},
+				{X: 0, Y: 1},
+				{X: 1, Y: 1},
+				{X: 1, Y: 0},
+			},
+		},
+	}
+	expected := 1.0
+
+	actual := getPolygonArea(polygonData[0].Coords)
+	if actual != expected {
+		t.Errorf("Expected %f, got %f", expected, actual)
+	}
+}
+
+func TestArea2(t *testing.T) {
+	polygonData := []dtoschema.PolygonData{
+		{
+			Runner: "nihal",
+			Coords: []dtoschema.CoordinateStruct{
+				{X: 0, Y: 0},
+				{X: 1, Y: -1},
+				{X: 2, Y: 0},
+				{X: 3, Y: 1},
+				{X: 2, Y: 2},
+				{X: 1, Y: 3},
+				{X: 0, Y: 2},
+				{X: -1, Y: 1},
+			},
+		},
+	}
+	expected := 8.0
+
+	actual := getPolygonArea(polygonData[0].Coords)
+	if actual != expected {
+		t.Errorf("Expected %f, got %f", expected, actual)
+	}
+}
+
+func TestArea3(t *testing.T) {
+	polygonData := []dtoschema.PolygonData{
+		{
+			Runner: "nihal",
+			Coords: []dtoschema.CoordinateStruct{
+				{X: -2, Y: -2},
+				{X: 0, Y: 0},
+				{X: 0, Y: 2},
+				{X: 2, Y: 0},
+				{X: 0, Y: 4},
+			},
+		},
+	}
+	expected := 6.0
+
+	actual := getPolygonArea(polygonData[0].Coords)
+	if actual != expected {
+		t.Errorf("Expected %f, got %f", expected, actual)
+	}
+}
